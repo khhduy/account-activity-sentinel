e1423a6ec590cee4c57bc9a3e3d31c9f90484bbd
kzduy <cookiezi2507@gmail.com>
2023-09-19 17:04:31 +0700

    init project

---
+++ activity-sentinel/src/main/java/com/kzduy/activitysentinel/ActivitySentinelApplication.java
@@ -0,0 +1,13 @@
+package com.kzduy.activitysentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitySentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitySentinelApplication.class, args);
+	}
+
+}
---
+++ activity-sentinel/src/test/java/com/kzduy/activitysentinel/ActivitySentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kzduy.activitysentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitySentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
