ac6b626febb639d8815ac8abd044070930e1432f
s-white-owl <cookiezi2507@gmail.com>
2023-10-16 18:45:24 +0700

    abandoned

---
+++ activity-sentinel/src/main/java/com/kzduy/activitysentinel/configuration/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.kzduy.activitysentinel.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return null;
+    }
+}
